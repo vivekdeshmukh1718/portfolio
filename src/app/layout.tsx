28d475a61bad47ee7ffe892b744706199e596404
vikkie <vivekdeshmukh1718@gmail.com>
2025-06-06 19:05:19 +0530

    genkit update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider defaultTheme="system">
           {children}
           <Toaster />
         </ThemeProvider>
